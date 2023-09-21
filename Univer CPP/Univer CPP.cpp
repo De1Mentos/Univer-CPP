67509f86aef9986f93e4bc205c4b85cf2abb33d9
De1Mentos <zofinlel@gmail.com>
2023-09-21 14:45:17 +0300

    qq

--- Univer CPP/Univer CPP.cpp
+++ Univer CPP/Univer CPP.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
 
-int main()
-{
-    int x = 1;
-    std::cout << x - 3;
+int main() {
+    double usd_to_uah = 27.65;
+
+    double eur_to_uah = 33;
+
+    std::cout << "Dollars\tGrivnas\tEuros\tGrivnas" << std::endl;
+
+    for (int i = 1; i <= 10; ++i) {
+        double dollars = i;
+        double euros = i * eur_to_uah / usd_to_uah;
+        std::cout << dollars << "\t" << dollars * usd_to_uah << "\t" << euros << "\t" << euros * usd_to_uah << std::endl;
+    }
+
+    return 0;
 }

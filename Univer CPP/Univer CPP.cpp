7d60e82ba4e9a61b5d35b3ba7b4ed32c9f7d5ed7
De1Mentos <zofinlel@gmail.com>
2023-10-12 19:09:07 +0300

    qq

--- Univer CPP/Univer CPP.cpp
+++ Univer CPP/Univer CPP.cpp
@@ -1,21 +1,44 @@
-﻿// Univer CPP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstdlib>  
+#include <ctime>   
 
-#include <iostream>
+int main() {
+    int n;
+    std::cout << "Amount of elements (n < 50): ";
+    std::cin >> n;
 
-int main()
-{
-    int x = 1;
-    std::cout << x - 3;
+    if (n < 1 || n >= 50) {
+        std::cout << "Error. Number should be in between 1 to 49" << std::endl;
+        return 1;
+    }
+
+    int* arr = new int[n];
+
+    std::srand(static_cast<unsigned>(std::time(0)));
+
+    std::cout << "Massive with " << n << " number was created: ";
+    for (int i = 0; i < n; i++) {
+        arr[i] = std::rand() % 2;
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    bool strictAlternation = true;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            strictAlternation = false;
+            break;
+        }
+    }
+
+    if (strictAlternation) {
+        std::cout << "Cherguvannia True -  0 and 1." << std::endl;
+    }
+    else {
+        std::cout << "Cherguvannia False - 0 and 1." << std::endl;
+    }
+
+    delete[] arr;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.

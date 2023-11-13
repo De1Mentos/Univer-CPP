33ce657128b74bf32defb3fb6efd03f02b94300a
De1Mentos <zofinlel@gmail.com>
2023-11-13 10:59:17 +0200

    qq

--- Univer CPP/Univer CPP.cpp
+++ Univer CPP/Univer CPP.cpp
@@ -1,21 +1,45 @@
-﻿// Univer CPP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
 
-#include <iostream>
+struct HistoricalDate {
+    int year;
+    std::string event;
 
-int main()
-{
-    int x = 1;
-    std::cout << x - 3;
+    HistoricalDate(int y, const std::string& e) : year(y), event(e) {}
+};
+
+double calculateAverageInterval(const std::vector<HistoricalDate>& dates) {
+    if (dates.size() < 2) {
+        std::cerr << "NOT ENOUGH DATES.\n";
+        return 0.0;
+    }
+
+    double totalInterval = 0.0;
+    for (size_t i = 1; i < dates.size(); ++i) {
+        int interval = std::abs(dates[i].year - dates[i - 1].year);
+        totalInterval += interval;
+    }
+
+    // Calculate the average interval
+    double averageInterval = totalInterval / (dates.size() - 1);
+    return averageInterval;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main() {
+    // Create a vector of historical dates
+    std::vector<HistoricalDate> historicalDates = {
+        {1776, "Declaration of Independence of the United States"},
+        {1789, "French Revolution"},
+        {1865, "End of the American Civil War"},
+        {1914, "Start of World War I"},
+        {1945, "End of World War II"}
+    };
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    double avgInterval = calculateAverageInterval(historicalDates);
+    std::cout << "Average interval between historical dates: " << avgInterval << " years\n";
+
+    return 0;
+
+}

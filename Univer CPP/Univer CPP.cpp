1fb8706af9981cb8a74b14b232c23f6edf30d9e5
De1Mentos <zofinlel@gmail.com>
2023-12-14 10:47:33 +0200

    qq

--- Univer CPP/Univer CPP.cpp
+++ Univer CPP/Univer CPP.cpp
@@ -1,30 +1,30 @@
 ﻿#include <SFML/Graphics.hpp>
-#pragma comment(lib, "sfml-system-d.libkernel.lib")
+
+using namespace sf;
 
 int main()
 {
-	// Создаем окно размером 600 на 600 и частотой обновления 60 кадров в секунду
-	sf::RenderWindow window(sf::VideoMode(600, 600), "15");
-	window.setFramerateLimit(60);
 
-	sf::Event event;
+    RenderWindow window(VideoMode(400, 400), L"New Project", Style::Default);
 
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) window.close();
-			if (event.type == sf::Event::KeyPressed)
-			{
-				// Получаем нажатую клавишу - выполняем соответствующее действие
-				if (event.key.code == sf::Keyboard::Escape) window.close();
-			}
-		}
+    window.setVerticalSyncEnabled(true);
 
-		// Выполняем необходимые действия по отрисовке
-		window.clear();
-		window.display();
-	}
+    CircleShape shape(100.f, 3);
+    shape.setPosition(100, 100);
+    shape.setFillColor(Color::Magenta);
 
-	return 0;
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color::Blue);
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
 }
